f0f727cffb500b4e5c68b70d1d5671cc0691aca8
Suomi228 <bixep345@gmail.com>
2024-07-31 12:07:07 +0300

    init

---
+++ photo-collection/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Collection from './components/Collection';
+
+function App() {
+  return (
+    <div className="App">
+      <Collection />
+    </div>
+  );
+}
+
+export default App;
