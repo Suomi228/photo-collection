eb4ad255b2b5b375da02498c98494fc46aa39834
Suomi228 <bixep345@gmail.com>
2024-07-31 14:50:19 +0300

    nav bar add

--- photo-collection/src/App.jsx
+++ photo-collection/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Collection from './components/Collection';
-
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <Collection />
+    <div className="app">
+        <h1>Моя коллекция фото</h1>
+      <div className='top'>
+        <ul className='tags'>
+          <li>Испания</li>
+          <li>Франция</li>
+          <li>Германия</li>
+          <li>Корея</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- photo-collection/src/components/Collection.jsx
+++ photo-collection/src/components/Collection.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import './Collection.css'
 function Collection() {
+
+
+  const [photos, setPhotos] = React.useState([]);
 
     const fetchPhotos = async ()=>{
       try {
           const response = await fetch(`https://66a9f428613eced4eba6f405.mockapi.io/api/v1/collections`)
           const data = await response.json()
           console.log(data)
+          setPhotos(data);
       } catch (error) {
         console.error('Error fetching photos:', error)
       }
@@ -17,7 +21,8 @@ function Collection() {
     }, [])
   return (
     <div>
-      <p>Hello world!</p>
+    
+
     </div>
   )
 }
