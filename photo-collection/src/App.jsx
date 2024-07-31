8a888b5e41aca7e69e2b8dc6e71d802d5e61ff88
Suomi228 <bixep345@gmail.com>
2024-07-31 19:21:32 +0300

    data fetching ui

--- photo-collection/src/App.jsx
+++ photo-collection/src/App.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import Collection from "./components/Collection";
 import "./index.css";
 function App() {
+  const [collections, setCollections] = React.useState([]);
+  const fetchPhotos = async ()=>{
+    try {
+        const response = await fetch(`https://66a9f428613eced4eba6f405.mockapi.io/api/v1/collections`)
+        const data = await response.json()
+        console.log(data)
+        setCollections(data);
+    } catch (error) {
+      console.error('Error fetching photos:', error)
+    }
+  }
+
+  React.useEffect(() => {
+      fetchPhotos()
+  }, [])
   return (
     <div className="app">
       <h1>Моя коллекция фото</h1>
       <div className="top">
         <ul className="tags">
+          <li className="active">Все</li>
           <li>Испания</li>
           <li>Франция</li>
           <li>Германия</li>
@@ -14,16 +30,15 @@ function App() {
         </ul>
       </div>
       <div className="content">
-        <Collection
-          name="Испания"
-          images={[
-            "https://i.natgeofe.com/k/e800ca90-2b5b-4dad-b4d7-b67a48c96c91/spain-madrid_16x9.jpg?w=1200",
-            "https://images.unsplash.com/photo-1476786516785-c3ff2dac9bbf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            "https://images.unsplash.com/photo-1558642084-fd07fae5282e?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            "https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          ]}
-        />
+        {collections.map((obj, index)=>
+        <Collection key = {index} name={obj.name}
+        images= {obj.photos}/>)}
       </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
     </div>
   );
 }
--- photo-collection/src/components/Collection.jsx
+++ photo-collection/src/components/Collection.jsx
@@ -5,20 +5,6 @@ function Collection({name, images}) {
 
   const [photos, setPhotos] = React.useState([]);
 
-    // const fetchPhotos = async ()=>{
-    //   try {
-    //       const response = await fetch(`https://66a9f428613eced4eba6f405.mockapi.io/api/v1/collections`)
-    //       const data = await response.json()
-    //       console.log(data)
-    //       setPhotos(data);
-    //   } catch (error) {
-    //     console.error('Error fetching photos:', error)
-    //   }
-    // }
-
-    // React.useEffect(() => {
-    //     fetchPhotos()
-    // }, [])
   return (
     <div className="collection">
       <img className="collection__big" src={images[0]} alt="Item" />
