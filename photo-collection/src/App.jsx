3f5ab5fedcacdb9f2bd6b91c666e5ff02f9ced28
Suomi228 <bixep345@gmail.com>
2024-07-31 16:04:01 +0300

    collection ui

--- photo-collection/src/App.jsx
+++ photo-collection/src/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import Collection from './components/Collection';
-import './index.css';
+import React from "react";
+import Collection from "./components/Collection";
+import "./index.css";
 function App() {
   return (
     <div className="app">
-        <h1>Моя коллекция фото</h1>
-      <div className='top'>
-        <ul className='tags'>
+      <h1>Моя коллекция фото</h1>
+      <div className="top">
+        <ul className="tags">
           <li>Испания</li>
           <li>Франция</li>
           <li>Германия</li>
           <li>Корея</li>
         </ul>
+      </div>
+      <div className="content">
+        <Collection
+          name="Испания"
+          images={[
+            "https://i.natgeofe.com/k/e800ca90-2b5b-4dad-b4d7-b67a48c96c91/spain-madrid_16x9.jpg?w=1200",
+            "https://images.unsplash.com/photo-1476786516785-c3ff2dac9bbf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1558642084-fd07fae5282e?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          ]}
+        />
       </div>
     </div>
   );
--- photo-collection/src/components/Collection.jsx
+++ photo-collection/src/components/Collection.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import './Collection.css'
-function Collection() {
+function Collection({name, images}) {
 
 
   const [photos, setPhotos] = React.useState([]);
 
-    const fetchPhotos = async ()=>{
-      try {
-          const response = await fetch(`https://66a9f428613eced4eba6f405.mockapi.io/api/v1/collections`)
-          const data = await response.json()
-          console.log(data)
-          setPhotos(data);
-      } catch (error) {
-        console.error('Error fetching photos:', error)
-      }
-    }
+    // const fetchPhotos = async ()=>{
+    //   try {
+    //       const response = await fetch(`https://66a9f428613eced4eba6f405.mockapi.io/api/v1/collections`)
+    //       const data = await response.json()
+    //       console.log(data)
+    //       setPhotos(data);
+    //   } catch (error) {
+    //     console.error('Error fetching photos:', error)
+    //   }
+    // }
 
-    React.useEffect(() => {
-        fetchPhotos()
-    }, [])
+    // React.useEffect(() => {
+    //     fetchPhotos()
+    // }, [])
   return (
-    <div>
-    
-
+    <div className="collection">
+      <img className="collection__big" src={images[0]} alt="Item" />
+      <div className="collection__bottom">
+        <img className="collection__mini" src={images[1]} alt="Item" />
+        <img className="collection__mini" src={images[2]} alt="Item" />
+        <img className="collection__mini" src={images[3]} alt="Item" />
+      </div>
+      <h4>{name}</h4>
     </div>
   )
 }
