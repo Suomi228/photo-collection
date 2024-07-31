51038712ace48c2ccaa56684e074719b1770a06c
Suomi228 <bixep345@gmail.com>
2024-07-31 12:18:37 +0300

    api call

--- photo-collection/src/components/Collection.jsx
+++ photo-collection/src/components/Collection.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 function Collection() {
+
+    const fetchPhotos = async ()=>{
+      try {
+          const response = await fetch(`https://66a9f428613eced4eba6f405.mockapi.io/api/v1/collections`)
+          const data = await response.json()
+          console.log(data)
+      } catch (error) {
+        console.error('Error fetching photos:', error)
+      }
+    }
+
+    React.useEffect(() => {
+        fetchPhotos()
+    }, [])
   return (
     <div>
       <p>Hello world!</p>
